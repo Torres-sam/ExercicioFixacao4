06156e56a47db75f358a7119ecc176d42f3bc90f
Samuel Torres <samuelvianatorres@gmail.com>
2025-08-07 21:29:56 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacao4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp
+{
+    internal class ExercicioFixacao4
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, r1, r2;
+
+            Console.WriteLine("Digite o valor de A:");
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de B:");
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de C:");
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            delta = Math.Pow(b, 2) - 4 * a * c;
+
+            if (a == 0 || delta < 0.0)
+            {
+                Console.WriteLine("Impossivel de Calcular !!!");
+            }
+            else
+            {
+                r1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                r2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+
+            }
+        }
+    }
+}
